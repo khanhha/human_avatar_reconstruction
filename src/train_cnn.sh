ed82a33d02c59adf53858ea208435d08c2981329
khanh <huy.khanh.ha@uni-weimar.de>
2019-09-13 14:27:32 +0200

    training: export all front, side and joint models to tensorflow

--- src/train_cnn.sh
+++ src/train_cnn.sh
@@ -2,7 +2,7 @@
 export PYTHONPATH="${PYTHONPATH}:./"
 DATA_DIR=$1
 
-if [$2 == ""]
+if [ $2 == "" ]
 then
   POSE_VARIANT=0
 else
@@ -26,47 +26,47 @@ else
     exit
 fi
 
-# echo '\n\nstart training front model'
-# python ./pca/nn_vic_train.py -root_dir $DATA_DIR -target_dir $TARGET_DIR -model_type f -height_path $HEIGHT_PATH -pca_model_path $PCA_MODEL_PATH    \
-#         -is_scale_target 1  \
-#         -is_scale_height 1 \
-#         -use_height 1 \
-#         -use_gender 1 \
-#         -num_classes 51 \
-#         -n_epoch 30 \
-#         -n_pose_variant $POSE_VARIANT
-#
-# echo '\n\nstart training side model'
-# python ./pca/nn_vic_train.py -root_dir $DATA_DIR -target_dir $TARGET_DIR -model_type s -height_path $HEIGHT_PATH -pca_model_path $PCA_MODEL_PATH    \
-#         -is_scale_target 1  \
-#         -is_scale_height 1 \
-#         -use_height 1 \
-#         -use_gender 1 \
-#         -num_classes 51 \
-#         -n_epoch 30 \
-#         -n_pose_variant $POSE_VARIANT
-#
-#echo '\n\nstart training joint model'
-#python ./pca/nn_vic_train.py -root_dir $DATA_DIR -target_dir $TARGET_DIR -model_type joint -height_path $HEIGHT_PATH -pca_model_path $PCA_MODEL_PATH    \
-#        -is_scale_target 1  \
-#        -is_scale_height 1 \
-#        -use_height 1 \
-#        -use_gender 1 \
-#        -num_classes 51 \
-#        -n_epoch 30 \
-#        -n_pose_variant $POSE_VARIANT
+echo '\n\nstart training front model'
+python ./pca/nn_vic_train.py -root_dir $DATA_DIR -target_dir $TARGET_DIR -model_type f -height_path $HEIGHT_PATH -pca_model_path $PCA_MODEL_PATH    \
+        -is_scale_target 1  \
+        -is_scale_height 1 \
+        -use_height 1 \
+        -use_gender 1 \
+        -num_classes 51 \
+        -n_epoch 30 \
+        -n_pose_variant $POSE_VARIANT
+
+echo '\n\nstart training side model'
+python ./pca/nn_vic_train.py -root_dir $DATA_DIR -target_dir $TARGET_DIR -model_type s -height_path $HEIGHT_PATH -pca_model_path $PCA_MODEL_PATH    \
+        -is_scale_target 1  \
+        -is_scale_height 1 \
+        -use_height 1 \
+        -use_gender 1 \
+        -num_classes 51 \
+        -n_epoch 30 \
+        -n_pose_variant $POSE_VARIANT
+
+echo '\n\nstart training joint model'
+python ./pca/nn_vic_train.py -root_dir $DATA_DIR -target_dir $TARGET_DIR -model_type joint -height_path $HEIGHT_PATH -pca_model_path $PCA_MODEL_PATH    \
+        -is_scale_target 1  \
+        -is_scale_height 1 \
+        -use_height 1 \
+        -use_gender 1 \
+        -num_classes 51 \
+        -n_epoch 30 \
+        -n_pose_variant $POSE_VARIANT
 
 echo '\n\nconvert pytorch pretrained weight to tensorflow graph'
 IN_MODEL_PATH="$DATA_DIR/models/joint/final_model.pt"
 OUT_MODEL_PATH="$DATA_DIR/models/shape_model.jlb"
 VIC_MESH_PATH="$DATA_DIR/victoria_caesar_template.obj"
 
-#if test -f "$IN_MODEL_PATH"; then
-#    echo 'start convert model: ' $IN_MODEL_PATH
-#    python ./pca/tool_convert_torch_to_tf.py -in_model_path $IN_MODEL_PATH -out_model_path $OUT_MODEL_PATH  -vic_mesh_path $VIC_MESH_PATH
-#else
-#    echo 'something wrong. the model path does not exist: ' $IN_MODEL_PATH
-#fi
+if test -f "$IN_MODEL_PATH"; then
+    echo 'start convert model: ' $IN_MODEL_PATH
+    python ./pca/tool_convert_torch_to_tf.py -in_model_path $IN_MODEL_PATH -out_model_path $OUT_MODEL_PATH  -vic_mesh_path $VIC_MESH_PATH
+else
+    echo 'something wrong. the model path does not exist: ' $IN_MODEL_PATH
+fi
 
 
 echo '\n\nconvert side pytorch pretrained weight to tensorflow graph'
